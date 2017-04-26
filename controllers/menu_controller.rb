1156e4a0beedd85e86ff6774cce64b85a03a5da1
realtimschmidt <schmidt.tim.j@gmail.com>
2017-04-25 23:08:31 -0400

    Assignment 5 Address Bloc Menu Completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry by number"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_by_number
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -54,6 +59,17 @@ class MenuController
 
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry_by_number
+    print "Please supply an entry number: "
+    selection = gets.chomp.to_i
+    if selection <= address_book.entries.length && selection > 0
+      puts address_book.entries[selection - 1]
+    else
+      puts "Invalid entry"
+      view_entry_by_number
+    end
   end
 
   def create_entry
